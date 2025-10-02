f36cb8c13ec3d721603e3ea9cf7cebb0c65f7469
Catherine Pitsigavdaki <48240339+KatePitsi@users.noreply.github.com>
2025-10-02 08:12:58 +0000

    edits

--- js/app.js
+++ js/app.js
@@ -2,32 +2,3 @@
 const year = new Date().getFullYear();
 document.getElementById("year").textContent = year;
 
-// Dropdowns / Mega Menu / Cart
-document.querySelectorAll("[data-toggle]").forEach(btn => {
-  btn.addEventListener("click", e => {
-    const targetId = btn.getAttribute("data-toggle");
-    const target = document.getElementById(targetId);
-    target.classList.toggle("active");
-  });
-});
-
-// Tooltip
-const tooltipBtns = document.querySelectorAll(".tooltip-btn");
-tooltipBtns.forEach(btn => {
-  let tooltip;
-  btn.addEventListener("mouseenter", () => {
-    tooltip = document.createElement("div");
-    tooltip.className = "tooltip";
-    tooltip.innerText = btn.dataset.tooltip;
-    btn.appendChild(tooltip);
-  });
-  btn.addEventListener("mouseleave", () => {
-    if (tooltip) tooltip.remove();
-  });
-});
-
-// Native dialog
-const dialog = document.getElementById("myDialog");
-document.getElementById("openDialog").addEventListener("click", () => {
-  dialog.showModal();
-});
